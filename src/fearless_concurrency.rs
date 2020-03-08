2cf9cb8792bb51aee7d2ce9e1fa954e3ed582259
modulitos <you@example.com>
2020-03-07 16:45:29 -0800

    fearless_concurrency

---
+++ src/fearless_concurrency.rs
@@ -0,0 +1,167 @@
+pub mod threads {
+    use std::sync::mpsc;
+    use std::thread;
+    use std::time::Duration;
+
+    use std::boxed;
+    use std::error;
+    use std::result;
+
+    type Error = boxed::Box<dyn error::Error>;
+    type Result<T, E = Error> = result::Result<T, E>;
+
+    pub fn test() {
+        thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread!", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {} from the main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    }
+    pub fn test_join_handle() -> Result<()> {
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread!", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        // TODO: how to replace this with the ? operator??? Updating our Error type?
+        handle.join().unwrap();
+        for i in 1..5 {
+            println!("hi number {} from the main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+        Ok(())
+    }
+    pub fn test_moving_data() {
+        let v = vec![1, 2, 3];
+
+        let handle = thread::spawn(move || {
+            println!("Here's a vector: {:?}", v);
+        });
+
+        handle.join().unwrap();
+    }
+
+    pub fn test_channels() {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let s = String::from("hi");
+            tx.send(s).unwrap();
+        });
+        let received = rx.recv().unwrap();
+        println!("Got: {}", received);
+    }
+
+    pub fn test_multiple_messages() {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        // instead of calling `recv` on rx, we're treating it as an iterator. Therefore, it will
+        // wait for values until the channel is closed, which will end the iterator
+
+        for received in rx {
+            println!("Got: {}", received);
+        }
+    }
+
+    pub fn test_multiple_producers() {
+        let (tx, rx) = mpsc::channel();
+
+        let tx1 = mpsc::Sender::clone(&tx);
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+        });
+
+        for received in rx {
+            println!("Got: {}", received);
+        }
+    }
+
+    pub mod mutexes {
+        use super::*;
+        use std::sync::{Arc, Mutex};
+        // use create::fearless_concurrency::threads::*;
+
+        pub fn basic() {
+            let m = Mutex::new(5);
+
+            {
+                // blocks the thread until lock is freed.
+                // If the thread holding the lock has panicked, then lock() returns an Err
+
+                let mut num = m.lock().unwrap();
+                // MutexGuard implements Deref:
+                *num = 6;
+                // dropping the MutexGuard releases the lock automatically
+            }
+
+            println!("m = {:?}", m);
+        }
+
+        pub fn multi_thread() {
+            let counter = Arc::new(Mutex::new(0));
+            let mut handles = vec![];
+
+            for _ in 0..10 {
+                let counter = Arc::clone(&counter);
+                let handle = thread::spawn(move || {
+                    let mut num = counter.lock().unwrap();
+
+                    *num += 1;
+                });
+                handles.push(handle);
+            }
+
+            for handle in handles {
+                handle.join().unwrap();
+            }
+
+            println!("Result: {}", *counter.lock().unwrap());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 // import modules here so that they'll run in our test suite
 mod different_types_blog;
+mod fearless_concurrency;
 mod state_pattern_blog;
+use std::boxed;
+use std::error;
+use std::result;
 
-fn main() {}
+type Error = boxed::Box<dyn error::Error>;
+type Result<T, E = Error> = result::Result<T, E>;
+
+fn main() -> Result<()>{
+    fearless_concurrency::threads::mutexes::multi_thread();
+
+    Ok(())
+}
