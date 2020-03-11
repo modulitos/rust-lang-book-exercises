0578af6a4b1983855a9cfe456fbaa6d9a4f7301e
modulitos <you@example.com>
2020-03-10 18:59:18 -0700

    src/advanced_traits.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 mod different_types_blog;
 mod fearless_concurrency;
 mod state_pattern_blog;
+mod advanced_traits;
+mod smart_pointers;
 use std::boxed;
 use std::error;
 use std::result;
@@ -10,7 +12,13 @@ type Error = boxed::Box<dyn error::Error>;
 type Result<T, E = Error> = result::Result<T, E>;
 
 fn main() -> Result<()>{
-    fearless_concurrency::threads::mutexes::multi_thread();
+    // fearless_concurrency::threads::mutexes::multi_thread();
+    // advanced_traits::methods_same_names::colliding_methods::main();
+    // advanced_traits::methods_same_names::associated_functions::main();
+    // advanced_traits::super_traits::main();
+    // advanced_traits::newtype_pattern::main();
 
+    // smart_pointers::deref::main();
+    smart_pointers::deref::implicit_coercion::main();
     Ok(())
 }
