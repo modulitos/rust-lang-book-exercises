b856491ea4c223becd83908bb5885de3e05ee86a
modulitos <modulitos@users.noreply.github.com>
2020-08-02 03:20:00 -0700

    add some comments

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -11,20 +11,43 @@ struct Worker {
 }
 
 impl Worker {
-    // Takes a closure of code to run and sends it to the already running thread for execution.
+    // Using an Arc<Mutex>> because taking a job off the channel queue involves
+    // mutating the receiver, so the threads need a safe way to share and modify
+    // receiver; otherwise, we might get race conditions.
 
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
-
         Worker {
             id,
             handle: thread::spawn(move || {
                 loop {
+                    // Here, we first call lock on the receiver to acquire the mutex, and then we
+                    // call unwrap to panic on any errors. Acquiring a lock might fail if the mutex
+                    // is in a poisoned state, which can happen if some other thread panicked while
+                    // holding the lock rather than releasing the lock. In this situation, calling
+                    // unwrap to have this thread panic is the correct action to take.
 
-                    // Using an Arc<Mutex>> because taking a job off the channel queue involves
-                    // mutating the receiver, so the threads need a safe way to share and modify
-                    // receiver; otherwise, we might get race conditions.
+                    // Note that .lock acquires a mutex, blocking the current thread until it is
+                    // able to do so.
+
+                    // If we get the lock on the mutex, we call recv to receive a Job from the
+                    // channel. A final unwrap moves past any errors here as well, which might occur
+                    // if the thread holding the sending side of the channel has shut down, similar
+                    // to how the send method returns Err if the receiving side shuts down.
+
+                    // The call to recv blocks, so if there is no job yet, the current thread will
+                    // wait until a job becomes available. The Mutex<T> ensures that only one Worker
+                    // thread at a time is trying to request a job.
 
                     let job = receiver.lock().unwrap().recv().unwrap();
+
+
+                    // Note that the temporary MutexGuard returned from the lock method is dropped
+                    // as soon as the "let job =" statement ends. This ensures that the lock is held
+                    // during the call to recv, but it is released before the call to job(),
+                    // allowing multiple requests to be serviced concurrently.
+
+                    // AKA, we are freeing up other threads to access the receiver *before* the job
+                    // is run.
 
                     println!("thread {} received a new job.", id);
                     job();
@@ -71,6 +94,8 @@ impl ThreadPool {
         ThreadPool { workers, sender }
     }
 
+    /// Takes a closure of code to run and sends it to the already running thread for execution.
+
     pub fn execute<F>(&self, job: F)
     where
         // The type for F is taken from the method signature of thread::spawn() here:
@@ -82,13 +107,11 @@ impl ThreadPool {
 
         self.sender
             .send(Box::new(job))
-
             // We’re calling expect on send for the case that sending fails. This might happen if,
             // for example, we stop all our threads from executing, meaning the receiving end has
             // stopped receiving new messages. Currently, we can’t stop our threads from executing:
             // our threads continue executing as long as the pool exists. The reason we use unwrap
             // is that we know the failure case won’t happen, but the compiler doesn’t know that.
-
             .expect("Failed to send job to channel consumer");
     }
 }
