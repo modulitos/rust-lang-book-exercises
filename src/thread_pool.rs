c9fc62346448f885f8dfb0f3b2af7ccb26eee139
modulitos <modulitos@users.noreply.github.com>
2020-08-02 02:53:43 -0700

    use Arc and Mutex to pass receiver of mpsc channel

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::thread::JoinHandle;
 
@@ -7,37 +7,52 @@ use std::thread::JoinHandle;
 
 struct Worker {
     id: usize,
-    handle: JoinHandle<Job>,
+    handle: JoinHandle<()>,
 }
-
-struct Job {}
 
 impl Worker {
     // Takes a closure of code to run and sends it to the already running thread for execution.
 
-    fn new(id: usize) -> Self {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+
+        // taking a job off the channel queue involves mutating the receiver, so the threads need a safe way to share and modify receiver; otherwise, we might get race conditions
+
         Worker {
             id,
-            handle: thread::spawn(|| loop {}),
+            handle: thread::spawn(|| {
+                receiver;
+                loop {}
+            }),
         }
     }
 }
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<()>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job {}
+
 
 impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
         let mut workers = Vec::with_capacity(size);
-        let (sender, rx) = mpsc::channel();
+        let (sender, receiver) = mpsc::channel();
+
+        // the channel implementation that Rust provides is multiple producer, single consumer. This
+        // means we can’t just clone the consuming end of the channel to fix this code. Even if we
+        // could, that is not the technique we would want to use; instead, we want to distribute the
+        // jobs across threads by sharing the single receiver among all the workers.
+
+        let receiver = Arc::new(Mutex::new(receiver));
 
         for i in 0..size {
             // create some threads and store them in the vector
-            workers.push(Worker::new(i))
+            workers.push(Worker::new(i, Arc::clone(&receiver)));
+            // workers.push(Worker::new(i, receiver.clone()));
         }
 
         ThreadPool { workers, sender }
