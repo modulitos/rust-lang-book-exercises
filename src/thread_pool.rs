0d92b8f9c25bed22dd6b96e923b1fce48018a7dc
modulitos <modulitos@users.noreply.github.com>
2020-08-02 02:28:59 -0700

    create space for our threads

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,11 +1,22 @@
 use std::thread;
+use std::thread::JoinHandle;
+
 pub struct ThreadPool {
-    threads: usize,
+    threads: Vec<JoinHandle<()>>,
 }
 
 impl ThreadPool {
-    pub fn new(threads: usize) -> Self {
-        Self { threads }
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+            threads.push(thread::spawn(|| loop {}))
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F, T>(&self, closure: F)
@@ -15,6 +26,10 @@ impl ThreadPool {
         T: Send + 'static,
     {
         // println!("executing!");
+
+        // TODO: instead of spawning a thread here, we should pass in the closure to a thread from
+        // our threadpool.
+
         thread::spawn(closure);
     }
 }
