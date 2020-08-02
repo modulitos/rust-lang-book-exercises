547d35872e426cc22869cf21654b3fb41cab8412
modulitos <modulitos@users.noreply.github.com>
2020-08-02 00:11:30 -0700

    basic HTTP server with response

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,6 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread::Thread;
 
 fn main() {
     // bind to our localhost, at port 7878 (which is "rust" when typed into a phone)
@@ -10,21 +11,43 @@ fn main() {
         let stream = stream.unwrap();
         println!("Connection established!");
         handle_connection(stream);
-
     }
 }
 
+/// Handles a HTTP request, and returns a response. Both the request and response are read and
+/// written from/to the TCP streeam.
+///
+/// HTTP is a text-based protocol, and a request takes this format:
+///
+/// Method Request-URI HTTP-Version CRLF
+/// headers CRLF
+/// message-body
+///
+/// Responses have the following format:
+///
+/// HTTP-Version Status-Code Reason-Phrase CRLF
+/// headers CRLF
+/// message-body
+
 fn handle_connection(mut stream: TcpStream) {
-    let mut buf = [0; 256];
+    // We are using 1024 here, because something shorter like 256 wouldn't be able to read the
+    // entire request made from a browser, given the extra headers.
+
+    // let mut buf = [0; 1024];
+    let mut buf = [0; 10];
+    // let mut buf = [0; 256];
 
     // Read the tcp stream. If accessed from a browser or curl, this should set the following value
-    // into buf:
+    // into our buf:
 
     // GET / HTTP/1.1
     // Host: localhost:7878
     // User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0
     // Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
     // Accept-Language: en-US,en;q=0.5
+    // Accept-Encoding: gzip, deflate
+    // Connection: keep-alive
+    // Upgrade-Insecure-Requests: 1
 
     // or with `curl http://localhost:7878/hey`
     // GET /hey HTTP/1.1
@@ -32,9 +55,34 @@ fn handle_connection(mut stream: TcpStream) {
     // User-Agent: curl/7.64.1
     // Accept: */*
 
-    stream
+    let bytes_read = stream
+        // WARNING: If the request contains more than buf.len() bytes, then we won't end up reading
+        // more than buf.len() bytes from the request, so the client will never get confirmation
+        // that the request was read. So once `stream` is dropped, the connection will be forcefully
+        // closed, resulting in a "connection reset" error in the client.
         .read(&mut buf)
         .expect("unable to read request into buffer");
 
-    println!("buf:\n{}", String::from_utf8_lossy(&buf));
+    println!("bytes read:{}", bytes_read);
+    println!("request buffer:\n{}\n", String::from_utf8_lossy(&buf[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    // The write method on stream takes a &[u8] and sends those bytes directly down the connection.
+
+    let bytes_written = stream
+        .write(response.as_bytes())
+        .expect("unable to write the response to the buffer.");
+
+    println!("bytes written:{}", bytes_written);
+
+    // Finally, flush will wait and prevent the program from continuing until all the bytes are
+    // written to the connection; TcpStream contains an internal buffer to minimize calls to the
+    // underlying operating system.
+
+    stream
+        .flush()
+        .expect("unable to write all bytes from the internal buffer to the connection.");
+
+    println!();
 }
