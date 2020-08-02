dfe6e571b032a0481dd3aeedf0ffadbe1fc649fc
modulitos <modulitos@users.noreply.github.com>
2020-08-02 02:41:06 -0700

    add initial mpsc channel and Job abstraction

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,12 +1,20 @@
+use std::sync::mpsc;
 use std::thread;
 use std::thread::JoinHandle;
 
+// Each Worker stores a single JoinHandle<()> instance. Each worker has an id so we can distinguish
+// between the different workers in the pool when logging or debugging.
+
 struct Worker {
     id: usize,
-    handle: JoinHandle<()>,
+    handle: JoinHandle<Job>,
 }
 
+struct Job {}
+
 impl Worker {
+    // Takes a closure of code to run and sends it to the already running thread for execution.
+
     fn new(id: usize) -> Self {
         Worker {
             id,
@@ -17,6 +25,7 @@ impl Worker {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<()>,
 }
 
 impl ThreadPool {
@@ -24,13 +33,14 @@ impl ThreadPool {
         assert!(size > 0);
 
         let mut workers = Vec::with_capacity(size);
+        let (sender, rx) = mpsc::channel();
 
         for i in 0..size {
             // create some threads and store them in the vector
             workers.push(Worker::new(i))
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F, T>(&self, closure: F)
